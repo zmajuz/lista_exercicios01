eeedcfa3897b6c65fd868ce95119d121aafc05fd
zmajuz <juliacabral61@gmail.com>
2024-08-16 19:12:19 -0300

    adicionando arquivo soma.rb

---
+++ soma.rb
@@ -0,0 +1,8 @@
+# Crie um script em Ruby que leia dois números inteiros e mostre a soma dos dois.
+
+def somar(n1, n2)
+    soma = n1 + n2
+    
+    p "A soma dos dois números é igual a: #{soma}"
+end
+somar(54, 15)
